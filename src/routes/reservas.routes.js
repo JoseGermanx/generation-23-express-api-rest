cd5ff651740f1fef2506c7d4f2927c3f5dc664b2
José Martínez <jgermanmm@gmail.com>
2026-04-14 13:03:01 -0400

    middleware para validar conflictos de reservas

--- src/routes/reservas.routes.js
+++ src/routes/reservas.routes.js
@@ -2,11 +2,12 @@
 const express = require("express");
 const { obtenerReservas, crearReserva } = require("../controllers/reservas.controllers");
 const validateReserva = require("../middlewares/validateReserva");
+const validarConflicto = require("../middlewares/validarConflicto");
 
 const router = express.Router();
 
 router.get("/", obtenerReservas);
-router.post("/",validateReserva, crearReserva);
+router.post("/",validateReserva, validarConflicto, crearReserva);
 
 
 module.exports = router
