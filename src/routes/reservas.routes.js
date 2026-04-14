fd8268ebb35ba340c69c5e0c8f1ab9954e73bdaf
José Martínez <jgermanmm@gmail.com>
2026-04-14 12:37:02 -0400

    implementación middleware validacion de datos

--- src/routes/reservas.routes.js
+++ src/routes/reservas.routes.js
@@ -1,11 +1,12 @@
 
 const express = require("express");
 const { obtenerReservas, crearReserva } = require("../controllers/reservas.controllers");
+const validateReserva = require("../middlewares/validateReserva");
 
 const router = express.Router();
 
 router.get("/", obtenerReservas);
-router.post("/", crearReserva);
+router.post("/",validateReserva, crearReserva);
 
 
 module.exports = router
