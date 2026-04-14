e774b5579073e0a525886a17ebbea6a7890e2c38
José Martínez <jgermanmm@gmail.com>
2026-04-14 12:10:36 -0400

    ejemplos de middlewares

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 // inicializar la aplicación de express
 
 const express = require("express");
-
+const morgan = require("morgan");
 const espaciosRoutes = require("./routes/espacios.routes")
 const reservarRoutes = require("./routes/reservas.routes")
 
@@ -9,7 +9,23 @@ const app = express();
 
 
 //middlewares
-app.use(express.json());
+app.use(express.json());  // -> Middleware incopodado
+app.use(morgan("dev")); /// _> Middleware de terceros
+
+app.use((req, res, next)=>{ // Middleware personalizado
+
+    const valido = true;
+    
+    if (valido) {
+         console.log("Datos válidos, avanza.")
+         next();
+    } else {
+        return res.status(400).json({
+            msg: "Datos invalidos"
+        })
+    }
+})
+
 
 // Rutas de espacios
 app.use("/espacios", espaciosRoutes);
