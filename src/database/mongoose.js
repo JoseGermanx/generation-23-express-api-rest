8cdaade4d64e667f8130bce53d8d9d3740a2c971
José Martínez <jgermanmm@gmail.com>
2026-04-26 23:49:53 -0400

    corregir variable de entorno para conexión a MongoDB

--- src/database/mongoose.js
+++ src/database/mongoose.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 async function connect() {
 
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(process.env.MONGODB_URI);
         console.log("Mongo corriendo correctamente.")
     } catch (error) {
         throw new Error(error)
